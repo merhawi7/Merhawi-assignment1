e8f1f797f75480506ce154e4e91fd15bb10fe500
merhawi7 <136537141+merhawi7@users.noreply.github.com>
2023-06-23 00:15:54 -0400

    Update Assignment2Ap.java

--- src/com/project/Assignment2Ap.java
+++ src/com/project/Assignment2Ap.java
@@ -4,9 +4,35 @@ public class Assignment2Ap {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner scanner = new Scanner(System.in);
 
+		String initialInvestmentInput = CollectInput(scanner, " what is the initial investment amount?");
+		String interstRateInput = CollectInput(scanner, "what is the interst Rate?");
+
+		double newBalance = Double.parseDouble(initialInvestmentInput);
+		double interstRate = Double.parseDouble(interstRateInput);
+		
+		for(int i = 0; i < 6; i++) {
+			calculateAndOutput(newBalance, interstRate, i);	
+		}
+		scanner.close();
+	}
+
+	private static double calculateAndOutput(double newBalance, double interstRate, int index) {
+		for (int i = 0; i < 5; i++) {
+			newBalance = newBalance * ( interstRate + 1.0);
+		}
+		System.out.println("after " +((index *5) + 5 )+ " years you would have " + Math.round (newBalance));
+		return newBalance;
 
 	}
 
-}
+	private static String CollectInput(Scanner scanner, String messegeToDisplayinConsole) {
+		System.out.println(messegeToDisplayinConsole);
+		String userInput = scanner.nextLine();
+		return userInput;
+	}
+
+}	
+
+
